3e4e1cf975f731a89eb713f0194fb06529594db0
yangf <yangfan02@flux.com.cn>
2024-06-08 16:17:52 +0800

    项目搭建

---
+++ authcenter/src/main/java/org/example/security/AuthCenterApplication.java
@@ -0,0 +1,13 @@
+package org.example.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCenterApplication.class, args);
+    }
+
+}
